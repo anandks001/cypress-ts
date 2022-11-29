a4eb28860279d80cf4154c00d54d493e4d0f48d5
anandks001 <inbha.anand@gmail.com>
2022-11-29 22:12:13 +0100

    Add config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'https://example.cypress.io',
+    viewportHeight: 720,
+    viewportWidth: 1280,
+    retries: {
+        "runMode": 2,
+        "openMode": 0
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
